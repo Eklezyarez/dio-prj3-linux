b5a42e7f895faf8b6f1bd32808bba436f68391a5
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-09-23 18:50:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 ini_set("display_errors", 1);
 header('Content-Type: text/html; charset=iso-8859-1');
 
-
-
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
 $servername = "192.168.1.100";
@@ -18,8 +16,6 @@ $password = "Senha543";
 $database = "bancoA";
 
 // Criar conexão
-
-
 $link = new mysqli($servername, $username, $password, $database);
 
 /* check connection */
@@ -32,9 +28,7 @@ $valor_rand1 =  rand(1, 999);
 $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
-
 $query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2')";
-
 
 if ($link->query($query) === TRUE) {
   echo "New record created successfully";
